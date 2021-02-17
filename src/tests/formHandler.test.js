ca9e1636dd4dbd7e931c205913a48cbf34b02e5e
sdkdeepa <deepa94086@gmail.com>
2021-02-16 19:08:13 -0800

    added jest

--- src/tests/formHandler.test.js
+++ src/tests/formHandler.test.js
@@ -1,9 +1,12 @@
 const handleSubmit = require("../client/js/formHandler");
-
+describe("Test for submission", () => {
 test('Form is submitted when the URL is valid', () => {
-    expect(handleSubmit("https://api.meaningcloud.com/sentiment-2.1?key=")).toBeTruthy();
-});
+    expect(handleSubmit("https://api.meaningcloud.com/sentiment-2.1?key=")).toBe(true);
+})
+})
 
+describe("Test for invalid URL", () => {
 test('Form is not submitted when the URL is invalid', () => {
-    expect(handleSubmit("Invalid URL")).toBeFalsy();
-});
+    expect(handleSubmit("Invalid URL, Please enter a valid one")).toBe(false);
+})
+})
