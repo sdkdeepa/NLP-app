4dbd5548ff37a59b0d707be6c970704486bf2676
desubram@adobe.com <desubram@adobe.com>
2021-02-18 23:49:03 -0800

    added babel and import for tests

--- src/__test__/checkForUrl.spec.js
+++ src/__test__/checkForUrl.spec.js
@@ -1,10 +1,10 @@
 // Reference: https://jestjs.io/docs/en/using-matchers , https://generouspiratequeen.tumblr.com/post/634113097308700672/tdd-automated-testing-in-javascript-using-jest
 
-import { checkForUrl } from "..src/client/js/checkForUrl"
+import { checkForUrl } from "../client/js/checkForUrl"
 
 describe("Test for Valid URL", () => {
     test('Form is submitted when the URL is valid', () => {
-        expect(checkForUrl("https://dev.to/carloswhite/how-to-reliably-install-node-js-using-nvm-on-macos-12j0")).tobe(true);
+        expect(checkForUrl("https://dev.to/carloswhite/how-to-reliably-install-node-js-using-nvm-on-macos-12j0")).toBe(true);
     })
 })
 
